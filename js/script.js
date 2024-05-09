fb3fd61e4bc679bfec1957a1573be0133c77ce48
Hoang Thien Dang Nguyen <159185239+dang-2nguyen@users.noreply.github.com>
2024-05-09 18:38:54 +0000

    advfsdvs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Dang All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Dang
+// Created on: May 2024
 // This file contains the JS functions for index.html
+
+function checking() {
+  const multiplier = parseInt(document.getElementById("multiplier").value)
+  const multiplied = parseInt(document.getElementById("multiplied").value)
+  let counter = 0
+  let number = 0
+  while(counter < multiplied) {
+     number += 0 + multiplier
+     counter++    
+  }
+  document.getElementById("result").innerHTML = number
+}
